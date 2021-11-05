2dfb6f0c33d7e5e7d6cc05eed4c914d79d1297e4
minosdavi <minosdavilima@gmail.com>
2021-11-04 21:59:16 -0300

    GET API value and compare to the user attempt

---
+++ Js/Main.js
@@ -0,0 +1,14 @@
+console.log('1')
+
+const number_compare = async() =>{
+    document.getElementById("number_user")
+            .addEventListener("focusout")
+    
+    
+    console.log('1')
+
+}
+
+function click_number_button(){
+    document.getElementById('number_user_button')
+}
